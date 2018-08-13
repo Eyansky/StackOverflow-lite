f9908f04cce55cbd20db50f8e048314811564ab3
eyansky <ianeyansky@gmail.com>
2018-08-13 18:12:12 +0300

    [chore #159733803] Initial setup

---
+++ Api/run.py
@@ -0,0 +1,13 @@
+"""
+Entry point of the app
+Runs the app in specified exported mode
+"""
+import os # allow workings of underlieing os
+
+# import the APP used to instanciate flask
+from api.server import APP
+
+# run the app
+
+if __name__ == '__main__':
+    APP.run()
