efaee4650fa99b87726fddf58dea68cdeab1a04e
eyansky <ianeyansky@gmail.com>
2018-08-15 17:09:12 +0300

    :bug:[Bug #159729375] changing import path

--- Api/run.py
+++ Api/run.py
@@ -5,7 +5,7 @@ Runs the app in specified exported mode
 import os # allow workings of underlieing os
 
 # import the APP used to instanciate flask
-from api.server import APP
+from api import APP
 
 # run the app
 
