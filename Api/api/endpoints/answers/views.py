b13c07dc50c52cde5706ddf69c0e4ef2d8ff67e4
eyansky <ianeyansky@gmail.com>
2018-08-16 22:15:57 +0300

    :zap: [Feature 159733666] adding answers endpoint

---
+++ Api/api/endpoints/answers/views.py
@@ -0,0 +1,54 @@
+#! /api/endpoints/answers/views.py
+# -*- coding: utf-8 -*-
+"""
+This is the answers module
+
+This module contains various routes for the answers endpoint
+"""
+
+from flask import Blueprint, request, make_response, jsonify
+from flask.views import MethodView
+from api.endpoints.resources.models import (answers)
+
+# Create a blueprint
+ANSWERS_BLUEPRINT = Blueprint('answers', __name__, url_prefix='/api/v1/users/')
+
+
+class addAnswer(MethodView):
+    """
+    Add answer
+    """
+
+    def is_valid(self, item):
+        """
+            checking for valid credentials
+        """
+        errors = {}
+        if not item.get("answer"):
+            errors['answer'] = "Answer is required"
+
+        return len(errors) == 0, errors
+
+    def post(self, id):
+        """add an answer"""
+        if request.is_json:
+            valid, errors = self.is_valid(request.json)
+            if not valid:
+                return {"data": errors, "status": "error"}, 400
+            #
+            result = request.json
+            jibu = result['answer']
+
+            answers(id, jibu)
+            return jsonify({"message": "answers has been added!!"})
+
+
+# define API resources
+ANSWERS_VIEW = addAnswer.as_view('answers_api')
+
+# add rules for answers endpoints
+ANSWERS_BLUEPRINT.add_url_rule(
+    '/questions/<int:id>/answers',
+    view_func=ANSWERS_VIEW,
+    methods=['POST']
+)
