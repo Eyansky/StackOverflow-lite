b2534502580221cd6377f6d0ffb597336859dba8
eyansky <ianeyansky@gmail.com>
2018-08-16 21:04:06 +0300

    :zap:[Feature #159730166] post question endpoint

--- Api/api/endpoints/questions/views.py
+++ Api/api/endpoints/questions/views.py
@@ -9,7 +9,7 @@ This module contains various routes for the questions endpoint
 from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
 from api.endpoints.resources.models import (
-    view_questions)
+    view_questions, addQuestion)
 
 # Create a blueprint
 QUESTIONS_BLUEPRINT = Blueprint('questions', __name__, url_prefix='/api/v1/users')
@@ -17,9 +17,39 @@ QUESTIONS_BLUEPRINT = Blueprint('questions', __name__, url_prefix='/api/v1/users
 class Questions(MethodView):
     """Questions """
 
+    def is_valid(self, item):
+        """
+            checking for valid credentials
+        """
+        errors = {}
+        if not item.get("title"):
+            errors['title'] = "Title is required"
+
+        if not item.get("question"):
+            errors['question'] = "Question is required"
+        elif len(item.get("question")) < 15:
+            errors["question"] = "Question must have more than 15 characters. Enhance it please"
+
+        return len(errors) == 0, errors
+
     def get(self):
         return jsonify({'questions': view_questions()}), 200
-    
+
+    def post(self):
+        """add a question"""
+        if request.is_json:
+            valid, errors = self.is_valid(request.json)
+            if not valid:
+                return {"data": errors, "status": "error"}, 400
+            # 
+            result = request.json
+            title = result['title']
+            question = result['question']
+
+            addQuestion(title, question)
+            return jsonify({"message": "Question has been added!!"})
+
+
     
 
 # define API resources
