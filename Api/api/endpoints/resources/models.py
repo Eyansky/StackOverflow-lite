922622076acde5df6610ef6cdbfc5a9f6efc0463
eyansky <ianeyansky@gmail.com>
2018-08-16 22:14:36 +0300

    :zap: [Chore 159733666] add answer function

--- Api/api/endpoints/resources/models.py
+++ Api/api/endpoints/resources/models.py
@@ -1,27 +1,41 @@
+import random 
 """
 The Database
 """
 
 questions = [
     {
-        "id":0,
-        "question":"This is a question. Isnt it?"
+        "id": 0,
+        "title": "Topic 1",
+        "question": "This is a question. Isnt it?"
     },
     {
-        "id":1,
-        "question":"This is a question. Isnt it?"
+        "id": 1,
+        "title": "Topic 1",
+        "question": "This is a question. Isnt it?"
     }
 ]
 
-answers = []
+answer = [
+    {
+        "id": 0,
+        "answer": "This is an answer. Isnt it?"
+    },
+    {
+        "id": 1,
+        "answer": "This is an answer. Isnt it?"
+    }]
+
 
 def view_questions():
     return questions
+
 
 def singleQuestion(id):
     question = [question for question in questions if question["id"] == id]
 
     return question
+
 
 def addQuestion(title, question):
     id = random.randint(0, 7777777)
@@ -33,3 +47,7 @@ def addQuestion(title, question):
     return True
 
 
+def answers(id, jibu):
+    data = { "id" : id, "answer": jibu}
+    answer.append(data)
+    return True 
