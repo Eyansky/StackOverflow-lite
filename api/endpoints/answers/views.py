33324e0dfd5c6b3c80f1ed7f28c566b7641879f2
eyansky <ianeyansky@gmail.com>
2018-08-24 21:20:43 +0300

    [Chore #159923500] answers endpoints

--- api/endpoints/answers/views.py
+++ api/endpoints/answers/views.py
@@ -8,7 +8,7 @@ This module contains various routes for the answers endpoint
 
 from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
-from api.endpoints.resources.models import (answers)
+from api.endpoints.answers.models import (answers, updateanswers)
 
 # Create a blueprint
 ANSWERS_BLUEPRINT = Blueprint('answers', __name__, url_prefix='/api/v1/users/')
@@ -41,6 +41,34 @@ class addAnswer(MethodView):
 
             answers(id, jibu)
             return jsonify({"message": "answers has been added!!"}), 201
+
+class updateAnswer(MethodView):
+    """
+    Update answer
+    """
+
+    def is_valid(self, item):
+        """
+            checking for valid credentials
+        """
+        errors = {}
+        if not item.get("answer"):
+            errors['answer'] = "Answer is required"
+
+        return len(errors) == 0, errors
+
+    def put(self, id):
+        """add an answer"""
+        if request.is_json:
+            valid, errors = self.is_valid(request.json)
+            if not valid:
+                return {"data": errors, "status": "error"}, 400
+            #
+            result = request.json
+            jibu = result['answer']
+
+            updateanswers(id, jibu)
+            return jsonify({"message": "answers has been Updated!!"}), 201
 
 
 # define API resources
